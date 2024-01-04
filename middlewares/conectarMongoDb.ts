25eaea96cacb65c46a93d1dc518caaf20ed6844d
Felipe <felipealencar.0014@gmail.com>
2024-01-04 16:35:09 -0300

    mudança de pasta

--- middlewares/conectarMongoDb.ts
+++ middlewares/conectarMongoDb.ts
@@ -15,13 +15,13 @@ export const conectarMongoDb = (handler : NextApiHandler) =>
         // obter a variável de ambiente preenchida do .env
         const {DB_CONEXAO_STRING} = process.env;
 
-        // se a env estiver vazia, aborda o uso do sistema e avisa o programador
+        // se a env estiver vazia, aborta o uso do sistema e avisa o programador
         if(!DB_CONEXAO_STRING) {
             return res.status(500).json({ erro : 'ENV de configuração do banco, não informado!' });
         }
 
         mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
-        mongoose.connection.on('error', erro => console.log(`Ocorreu erro ao conectar no banco: ${erro}`))
+        mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error}`))
         await mongoose.connect(DB_CONEXAO_STRING);
 
         // agora posso seguir para o endpoint, pois estou conectado no banco
