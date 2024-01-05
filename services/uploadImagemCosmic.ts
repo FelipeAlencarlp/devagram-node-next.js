7a669d0a0551072f9bdc8cfc24ca0d544677f5e4
Felipe <felipealencar.0014@gmail.com>
2024-01-05 13:48:23 -0300

    endpoint publicação

---
+++ services/uploadImagemCosmic.ts
@@ -0,0 +1,43 @@
+import { createBucketClient } from '@cosmicjs/sdk';
+import multer from 'multer';
+
+const { BUCKET_SLUG, READY_KEY, WRITE_KEY } = process.env;
+
+const bucketDevagram = createBucketClient({
+    bucketSlug : BUCKET_SLUG as string,
+    readKey : READY_KEY as string,
+    writeKey : WRITE_KEY as string,
+});
+
+const storage = multer.memoryStorage();   
+const upload = multer({ storage : storage });
+
+const uploadImagemCosmic = async (req : any) => {
+    if (req?.file?.originalname) {
+        if (!req.file.originalname.includes(".png")
+            && !req.file.originalname.includes(".jpg")
+            && !req.file.originalname.includes(".jpeg")
+        ) {
+            throw new Error("Extensão da imagem inválida");
+        }
+
+        const media_object = {
+            originalname : req.file.originalname,
+            buffer : req.file.buffer,
+        };
+    
+        if (req.url && req.url.includes("publicacao")) {
+            return await bucketDevagram.media.insertOne({
+                media : media_object,
+                folder : "publicacao",
+            });
+        } else {
+            return await bucketDevagram.media.insertOne({
+                media : media_object,
+                folder : "avatar",
+            });
+        } 
+    }
+};
+    
+export { upload, uploadImagemCosmic };
---
+++ src/pages/api/publicacao.ts
@@ -0,0 +1,60 @@
+import type { NextApiResponse } from 'next';
+import type { RespostaPadraoMsg } from '../../../types/RespostaPadraoMsg';
+import nc from "next-connect";
+import { upload, uploadImagemCosmic } from '../../../services/uploadImagemCosmic';
+import { conectarMongoDb } from '../../../middlewares/conectarMongoDb';
+import { validarTokenJWT } from '../../../middlewares/validarTokenJWT';
+import { PublicacaoModel } from '../../../models/PublicacaoModel';
+import { UsuarioModel } from '../../../models/UsuarioModel';
+
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req : any, res : NextApiResponse<RespostaPadraoMsg>) => {
+        try{
+            const { userId } = req.query;
+            const usuario = await UsuarioModel.findById(userId);
+
+            if(!usuario) {
+                return res.status(400).json({ erro : 'Usuario nao encontrado' });
+            }
+
+            if(!req || !req.body) {
+                return res.status(400).json({ erro : 'Parametros de entrada nao informados'});
+            }
+            const { descricao } = req?.body;
+
+            if(!descricao || descricao.length < 2) {
+                return res.status(400).json({ erro : 'Descricao nao e valida' });
+            }
+    
+            if(!req.file || !req.file.originalname) {
+                return res.status(400).json({ erro : 'Imagem e obrigatoria' });
+            }
+
+            const image = await uploadImagemCosmic(req);
+            const publicacao = {
+                idUsuario : usuario._id,
+                descricao,
+                foto : image.media.url,
+                data : new Date()
+            };
+
+            usuario.publicacoes++;
+            await UsuarioModel.findByIdAndUpdate({ _id : usuario._id }, usuario);
+
+            await PublicacaoModel.create(publicacao);
+            return res.status(200).json({msg : 'Publicacao criada com sucesso'});
+        }catch(e){
+            console.log(e);
+            return res.status(400).json({erro : 'Erro ao cadastrar publicacao'});
+        }
+});
+
+export const config = {
+    api : {
+        bodyParser : false
+    }
+};
+
+// só vai conectar com a API se o token estiver validado e o banco de dados conectado
+export default validarTokenJWT(conectarMongoDb(handler));
